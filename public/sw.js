9d09128c72ab86d7a28eacf04797012f2ac4dc14
oguzdurak16-create <oguzdurak16@gmail.com>
2026-07-20 14:46:59 +0300

    Service worker önbelleğini alt yol uyumlu yap

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,8 @@
-const CACHE_NAME = 'hesaplas-v5-20260720'
-const APP_SHELL = ['/', '/tum-araclar/', '/offline/', '/manifest.webmanifest', '/logo-192.png', '/logo-512.png']
+const CACHE_NAME = 'hesaplas-v5-pages-20260720'
+const scopeUrl = new URL(self.registration.scope)
+const BASE_PATH = scopeUrl.pathname.replace(/\/$/, '')
+const withBase = (path) => `${BASE_PATH}${path}`
+const APP_SHELL = [withBase('/'), withBase('/tum-araclar/'), withBase('/offline/'), withBase('/manifest.webmanifest'), withBase('/logo-192.png'), withBase('/logo-512.png')]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()))
@@ -20,7 +23,7 @@ self.addEventListener('fetch', (event) => {
       const copy = response.clone()
       caches.open(CACHE_NAME).then((cache) => cache.put(request, copy))
       return response
-    }).catch(async () => (await caches.match(request)) || caches.match('/offline/')))
+    }).catch(async () => (await caches.match(request)) || caches.match(withBase('/offline/'))))
     return
   }
 
