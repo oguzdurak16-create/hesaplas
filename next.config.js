0ce6c003e2d85bd1aedfbd0028b589ffc61830da
oguzdurak16-create <oguzdurak16@gmail.com>
2026-07-20 14:46:09 +0300

    GitHub Pages için koşullu statik yayın yolu ekle

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+const basePath = isGithubPages ? '/hesaplas' : ''
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
   poweredByHeader: false,
+  basePath,
+  assetPrefix: basePath || undefined,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
 }
 
 module.exports = nextConfig
